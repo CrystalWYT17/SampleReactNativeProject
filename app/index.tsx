4b40f87de8fa5f8908eb604e69479da3bda2492b
CrystalWYT17 <wuttyeetun1721999@gmail.com>
2024-08-05 14:58:03 +0800

    React Navigation

--- app/index.tsx
+++ app/index.tsx
@@ -8,24 +8,49 @@ import Welcome from "@/components/Welcome";
 import WelcomeColorScheme from "@/components/WelcomeColorScheme";
 import WelcomeScreen from "@/components/WelcomeScreen";
 import { StyleSheet, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+// Instantiate Stack
+const Stack = createNativeStackNavigator();
 
 export default function HomeScreen() {
   return (
-    <>
+    // <>
+    //   <View style={styles.container}>
+    //     <LittleLemonHeader />
+    //     <WelcomeScreen />
+    //     {/* <MenuItems /> */}
+    //     {/* <MenuItemsFlatList /> */}
+    //     {/* <FeedbackForm /> */}
+    //     {/* <LoginScreen /> */}
+    //     {/* <Welcome /> */}
+    //     {/* <WelcomeColorScheme /> */}
+    //   </View>
+    //   <View style={styles.footer}>
+    //     <Footer />
+    //   </View>
+    // </>
+    <NavigationContainer independent={true}>
       <View style={styles.container}>
         <LittleLemonHeader />
-        <WelcomeScreen />
-        {/* <MenuItems /> */}
-        {/* <MenuItemsFlatList /> */}
-        {/* <FeedbackForm /> */}
-        {/* <LoginScreen /> */}
-        {/* <Welcome /> */}
-        {/* <WelcomeColorScheme /> */}
+        <Stack.Navigator
+          initialRouteName="Login"
+          screenOptions={{ headerStyle: { backgroundColor: "#FBDABB" } }}
+        >
+          <Stack.Screen
+            options={{ title: "Home" }}
+            name="Welcome"
+            component={WelcomeScreen}
+          />
+          <Stack.Screen name="Menu" component={MenuItemsFlatList} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+        </Stack.Navigator>
       </View>
       <View style={styles.footer}>
         <Footer />
       </View>
-    </>
+    </NavigationContainer>
   );
 }
 
